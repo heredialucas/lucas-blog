822c0967f1fa9d0ae6e0f4c99dbc7101a12e609e
heredialucas <heredialucasfac22@gmail.com>
2024-10-04 13:44:55 +0200

    nav fixed

--- src/app/(portfolio)/home/layout.jsx
+++ src/app/(portfolio)/home/layout.jsx
@@ -1,6 +1,6 @@
 export default function HomeLayout({ children }) {
   return (
-    <div className="flex flex-col justify-around h-[750px] m-12 md:mx-6 ">
+    <div className="flex flex-col md:justify-around h-[750px] m-12 md:mx-6 ">
       {children}
     </div>
   );
