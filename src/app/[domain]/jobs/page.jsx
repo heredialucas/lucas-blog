38effaccd741826bdbb45e5ee34a09b0a0a0b5c3
heredialucas <heredialucasfac22@gmail.com>
2024-12-02 00:54:02 +0100

    fix: timelinejobs

--- src/app/[domain]/jobs/page.jsx
+++ src/app/[domain]/jobs/page.jsx
@@ -5,6 +5,10 @@ import { TimelineJobs } from "./timelineJobs";
 export default async function TimelineJobsServer({ params }) {
   const { domain } = params;
 
+  if (!domain) {
+    return <></>;
+  }
+
   return (
     <Suspense fallback={<Loading />}>
       <TimelineJobs domain={domain} />
