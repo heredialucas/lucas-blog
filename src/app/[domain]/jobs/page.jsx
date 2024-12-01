24a62b1c7f068485319bd21edc1641ff7d465572
heredialucas <heredialucasfac22@gmail.com>
2024-12-02 00:57:15 +0100

    fix: timeline job

--- src/app/[domain]/jobs/page.jsx
+++ src/app/[domain]/jobs/page.jsx
@@ -2,12 +2,8 @@ import { Loading } from "@/components/custom/loading";
 import { Suspense } from "react";
 import { TimelineJobs } from "./timelineJobs";
 
-export default async function TimelineJobsServer({ params }) {
+export default function TimelineJobsServer({ params }) {
   const { domain } = params;
-
-  if (!domain) {
-    return <></>;
-  }
 
   return (
     <Suspense fallback={<Loading />}>
