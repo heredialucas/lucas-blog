fde2bbaf8b01ea9673e90d34f9d144a1da405758
heredialucas <heredialucasfac22@gmail.com>
2024-12-02 01:06:06 +0100

    fix: timeLine job

--- src/app/[domain]/jobs/page.jsx
+++ src/app/[domain]/jobs/page.jsx
@@ -1,6 +1,6 @@
 import { Loading } from "@/components/custom/loading";
 import { Suspense } from "react";
-import { TimelineJobs } from "./timelineJobs";
+import { TimelineJobs } from "./timeLineJobs";
 
 export default function TimelineJobsServer({ params }) {
   const { domain } = params;
